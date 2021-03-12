68d81b73a3c5a46fff420f1eddf2c7937f0a2863
Trishia <trishia1298@gmail.com>
2021-03-13 03:22:36 +0900

    envconfig was added in sysn/task/queue/config.go

--- sync/task/queue/config.go
+++ sync/task/queue/config.go
@@ -8,11 +8,11 @@ import (
 // Config configuration for task queue.
 type Config struct {
 	// GoroutinePoolSize maximum number of running goroutine instances.
-	GoroutinePoolSize int `yaml:"goroutinePoolSize" json:"goroutinePoolSize"`
+	GoroutinePoolSize int `yaml:"goroutinePoolSize" json:"goroutinePoolSize" envconfig:"GOROUTINE_POOL_SIZE"`
 	// Size size of queue. If it exhausted Submit returns error.
-	Size int `yaml:"size" json:"size"`
+	Size int `yaml:"size" json:"size" envconfig:"SIZE"`
 	// MaxIdleTime sets up the interval time of cleaning up goroutines, 0 means never cleanup.
-	MaxIdleTime time.Duration `yaml:"maxIdleTime" json:"maxIdleTime"`
+	MaxIdleTime time.Duration `yaml:"maxIdleTime" json:"maxIdleTime" envconfig:"MAX_IDLE_TIME"`
 }
 
 // SetDefaults set zero values to defautls.
